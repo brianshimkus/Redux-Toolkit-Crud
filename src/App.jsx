88513702e950a2f3cf578815e6a9acf3fcce76c1
Brian Shimkus <brianshimkus1@gmail.com>
2025-05-12 11:43:03 -0500

    Added styling fixes #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css'
 import { MovieInput } from './components/MovieInput'
 import { MovieList } from './components/MovieList'
 
 function App() {
 	return (
-		<>
-			<MovieInput />
-			<MovieList />
-		</>
+		<div className='bg-slate-900 min-h-screen text-slate-200 flex items-center justify-center'>
+			<div className='bg-slate-800 p-12 rounded-md border-2 border-slate-700'>
+				<MovieInput />
+				<hr className='my-4' />
+				<MovieList />
+			</div>
+		</div>
 	)
 }
 
--- src/components/MovieInput.jsx
+++ src/components/MovieInput.jsx
@@ -15,8 +15,17 @@ export const MovieInput = () => {
 	}
 	return (
 		<>
-			<input onChange={(e) => setNewMovie(e.target.value)} value={newMovie} />
-			<button onClick={handleAddMovie}>Add Movie</button>
+			<input
+				className='bg-slate-600 p-2 rounded-md border-2 border-slate-700 text-slate-200 outline-none focus:border-slate-500 w-full'
+				placeholder='Add a new movie'
+				onChange={(e) => setNewMovie(e.target.value)}
+				value={newMovie}
+			/>
+			<button
+				className='bg-green-400 hover:bg-green-300 text-green-800 font-bold py-2 px-4 rounded cursor-pointer transition duration-200 ease-in-out mt-2 w-full'
+				onClick={handleAddMovie}>
+				Add Movie
+			</button>
 		</>
 	)
 }
